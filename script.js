37399fad311dc4754ab687f1f00dc7f0e0ccd9e3
Austin Rohn <Austinrohn19@gmail.com>
2021-06-13 13:56:30 -0400

    start of password Generator

---
+++ script.js
@@ -0,0 +1,58 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  function generatePassword() {
+    var lengthPrompt = window.prompt("Please Choose between 8 and 128 charcters!");
+    if (lengthPrompt >= 8 && lengthPrompt <=128) {
+     var numberRequest = window.confirm("Should this Password include Numbers? OK= Yes Cancel=No");
+    }else {
+      window.alert("Not Valid Option, Please choose between 8 and 128 charcters!");
+    }
+    if(numberRequest === true || numberRequest == false){
+      var uppercaserequest = window.confirm("Should this password have Uppercase letters? OK= Yes Cancel=No");
+    }
+    if(numberRequest === true || numberRequest == false){
+      var lowercaserequest = window.confirm("Should this password have Lowercase letters? OK= Yes Cancel=No");
+    }
+    if(lowercaserequest === true || lowercaserequest == false){
+      var CharacterRequest = window.confirm("Should this password have Special Characters? OK= Yes Cancel=No");
+    }
+  };
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+function RandomLower() {
+  return String.fromCharCode(math.floor(math.random() * 26) + 97 );
+}
+
+function RandomUpper() {
+  return String.fromCharCode(math.floor(math.random() * 26) + 65 );
+}
+
+function Randomnumber() {
+  return String.fromCharCode(math.floor(math.random() * 10) + 48 );
+}
+
+function Randomcharacter() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
+
+var getrandom = {
+  lower: RandomLower,
+  upper: RandomUpper,
+  number: Randomnumber,
+  character: Randomcharacter
+};
+
+var typescount = numberRequest + lowercaserequest + uppercaserequest + CharacterRequest
+
+
+generateBtn.addEventListener("click", writePassword);
